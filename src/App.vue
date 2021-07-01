053b813878fce6ef649fce60c2b2e51097d6cd2a
hhhhhq <Mr.j@tom.com>
2021-07-01 10:10:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: hhhhhq
+ * @Date: 2021-07-01 09:34:16
+ * @LastEditors: hhhhhq
+ * @LastEditTime: 2021-07-01 09:42:18
+ * @Description: file content
+-->
+<template>
+  <TabsWrapper>
+    <Tab title="Tab1">Hello from Tab1</Tab>
+    <Tab title="Tab2">Hello from Tab2</Tab>
+    <Tab title="Tab3">Hello from Tab3</Tab>
+    <Tab title="Tab4">Hello from Tab4</Tab>
+  </TabsWrapper>
+</template>
+
+<script setup>
+import TabsWrapper from "./components/TabsWrapper.vue"
+import Tab from "./components/Tab.vue"
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Tab.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: hhhhhq
+ * @Date: 2021-07-01 09:36:01
+ * @LastEditors: hhhhhq
+ * @LastEditTime: 2021-07-01 10:07:58
+ * @Description: file content
+-->
+<template>
+  <div class="tab-content" v-show="title == selectedTitle">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { inject } from "vue"
+
+export default {
+  props: ["title"],
+  setup() {
+    const selectedTitle = inject("selectedTitle")
+
+    return {
+      selectedTitle,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.tab-content {
+  min-height: 400px;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 3px 5px 5px rgba(0, 0, 0, 0.05);
+}
+</style>
